96abd92f700f9fa48e6b5c983b2ac58a74aacda3
Hiroka Matsuda <mh1502b@gmail.com>
2018-10-02 14:48:00 +0900

    test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,17 @@
 class PostsController < ApplicationController
-    
-    
+
+
     def index
         @posts = Post.all.order(created_at: 'desc')
     end
-    
+
     def show
         @post = Post.find(params[:id])
     end
-    
+
     def new
-        
     end
-    
+
     def create
         #render plain: params[:post].inspect
         @post = Post.new(post_params)
@@ -23,10 +22,10 @@ class PostsController < ApplicationController
        render 'new'
         end
     end
-    
+
     private
     def post_params
         params.require(:post).permit(:title, :body)
     end
-    
+
 end
