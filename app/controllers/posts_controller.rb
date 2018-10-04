bb64666fc3d11e9d1a2f65627cefc08f6c28b245
Hiroka Matsuda <mh1502b@gmail.com>
2018-10-04 10:58:06 +0900

    renew

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
     end
 
     def new
+      @post = Post.new
     end
 
     def create
@@ -22,12 +23,31 @@ class PostsController < ApplicationController
        else
        #render plain: @post.errors.inspect
        render 'new'
+       end
+    end
+
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+          redirect_to posts_path
+        else
+          render 'edit'
         end
     end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+end
+
 
     private
     def post_params
         params.require(:post).permit(:title, :body)
     end
-
 end
